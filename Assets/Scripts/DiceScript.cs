05c803ead6b7a598dbee0a62cfd629997fabedc9
Sangam GC <sangamgc7@gmail.com>
2018-07-03 02:07:49 +0545

    First Roll of dice

---
+++ Assets/Scripts/DiceScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class DiceScript : MonoBehaviour
+{
+	public static DiceScript Instance { get; set; }
+
+	public static DiceScript GetInstance()
+	{
+		if (Instance == null)
+		{
+			return FindObjectOfType<DiceScript>();
+		}
+
+		return Instance;
+	}
+	public float forceAmount = 20f;
+	public float torqueAmount = 50f;
+	public bool roll;
+	public ForceMode forceMode;
+	private Transform[] _dice;
+	private float nextRoll;
+	private float RollRate = .5f;
+
+	private void Start()
+	{
+		_dice = GetComponentsInChildren<Transform>();
+		for (int i = 0; i < _dice.Length; i++)
+		{
+			Debug.Log(_dice[i].transform.position);
+		}
+		
+	}
+
+	public void Roll()
+	{
+		if (roll)
+			roll = false;
+		else
+			roll = true;
+		
+		
+	}
+
+	private void Update()
+	{
+		if (roll && Time.time>nextRoll)
+		{
+			nextRoll = Time.time + RollRate;
+			Debug.Log("Rolled");
+			for (int i = 0; i < 6; i++)
+			{
+				transform.GetChild(i).GetComponent<Rigidbody>().AddForce(Random.onUnitSphere*forceAmount, forceMode);
+				transform.GetChild(i).GetComponent<Rigidbody>().AddTorque(Random.onUnitSphere*torqueAmount,forceMode);
+			
+			}
+		}
+		
+	}
+}
+
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	
+	public static GameManager Instance { get; set; }
+
+	public static GameManager GetInstance()
+	{
+		if (Instance == null)
+		{
+			return FindObjectOfType<GameManager>();
+		}
+
+		return Instance;
+	}
+
+
+	//public bool Roll;
+
+	
+	public void RollDices()
+	{
+		GameObject.FindGameObjectWithTag("Dice").GetComponentInChildren<DiceScript>().Roll();
+	}
+}
