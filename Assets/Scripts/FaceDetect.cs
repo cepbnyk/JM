3cf52af5faf2faba567a59154367516c61e89b29
Sangam GC <sangamgc7@gmail.com>
2018-07-11 18:02:12 +0545

    Updated

---
+++ Assets/Scripts/FaceDetect.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceDetect : MonoBehaviour {
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.CompareTag("Jhanda"))
+		{
+			if (other.transform.parent.GetComponent<Rigidbody>().IsSleeping())
+			{
+				other.transform.parent.GetComponent<DiceScript>().DiceValue = 2;
+			}
+		}
+		else if (other.gameObject.CompareTag("Burja"))
+		{
+			if (other.transform.parent.GetComponent<Rigidbody>().IsSleeping())
+			{
+				other.transform.parent.GetComponent<DiceScript>().DiceValue = 1;
+			}
+		}
+		else if (other.gameObject.CompareTag("Itta"))
+		{
+			if (other.transform.parent.GetComponent<Rigidbody>().IsSleeping())
+			{
+				other.transform.parent.GetComponent<DiceScript>().DiceValue = 4;
+			}
+		}
+		else if (other.gameObject.CompareTag("Hukum"))
+		{
+			if (other.transform.parent.GetComponent<Rigidbody>().IsSleeping())
+			{
+				other.transform.parent.GetComponent<DiceScript>().DiceValue = 3;
+			}
+		}
+		else if (other.gameObject.CompareTag("Paan"))
+		{
+			if (other.transform.parent.GetComponent<Rigidbody>().IsSleeping())
+			{
+				other.transform.parent.GetComponent<DiceScript>().DiceValue = 6;
+			}
+		}
+		else if (other.gameObject.CompareTag("Chiddi"))
+		{
+			if (other.transform.parent.GetComponent<Rigidbody>().IsSleeping())
+			{
+				other.transform.parent.GetComponent<DiceScript>().DiceValue = 5;
+			}
+		}
+	}
+}
